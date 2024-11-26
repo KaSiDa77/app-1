c8248a3d96240412b5752c1cc2d3353f47cffcaa
KaSiDa77 <kabirsdayal@gmail.com>
2024-11-26 20:03:23 +0530

    components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Trees from './App.js';
+ReactDOM.render(<Trees/>, document.getElementById("root"))
+/*class Person extends React.Component{
+    render() {
+       return <h1>This is a person who is 55 years old.</h1>
+    }
+}
+ReactDOM.render(<Person/>, document.getElementById("root"))*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*function Hello(){
+    return <h1>This is a human from Earth. Hello.</h1>
+}
+ReactDOM.render(<Hello/>, document.getElementById("root"))*/
