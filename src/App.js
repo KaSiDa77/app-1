4ca63f68e0ad62e3d813ca7a5ed10208f17dccf8
KaSiDa77 <kabirsdayal@gmail.com>
2024-12-22 16:43:22 +0530

    Multiplier react app

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class Trees extends React.Component{
-  render(){
-    return <h1>There are many trees in India</h1>
-  }
-}
-export default Trees
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Trees from './App.js';
-ReactDOM.render(<Trees/>, document.getElementById("root"))
+
+
 /*class Person extends React.Component{
     render() {
        return <h1>This is a person who is 55 years old.</h1>
@@ -13,3 +11,96 @@ ReactDOM.render(<Person/>, document.getElementById("root"))*/
     return <h1>This is a human from Earth. Hello.</h1>
 }
 ReactDOM.render(<Hello/>, document.getElementById("root"))*/
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+class MyForm extends React.Component {
+
+constructor(props) {
+
+super(props);
+
+this.state = {
+
+result: 0
+
+};
+
+this.op1Ref = React.createRef();
+
+this.op2Ref = React.createRef();
+
+this.resultRef = React.createRef();
+
+}
+
+mysubmit = () => {
+
+const op1 = this.op1Ref.current.value;
+
+const op2 = this.op2Ref.current.value;
+
+this.resultRef.current.innerHTML = `Result: ${op1 * op2}`;
+
+}
+
+render() {
+
+return (
+
+<form>
+
+<div className="container">
+
+<h1 >Multiply</h1>
+
+<div className="form-group">
+
+<label> Enter Operand 1 </label>
+
+<input type="number" id="num1"
+
+ref={this.op1Ref}
+
+placeholder="Enter Operand 1" />
+
+</div>
+
+<div className="form-group">
+
+<label> Enter Operand 2 </label>
+
+<input type="number" id="num2"
+
+ref={this.op2Ref}
+
+placeholder="Enter Operand 2" />
+
+</div>
+
+<div>
+
+<p ref={this.resultRef}></p>
+
+</div>
+
+<div>
+
+<button type="button" className="btn" onClick={() => this.mysubmit()}>Multiply</button>
+
+</div>
+
+</div>
+
+</form>
+
+);
+
+}
+
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
